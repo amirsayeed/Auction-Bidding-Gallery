9bf412c73277f7846997625076e5429738959446
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-04-08 23:15:33 +0600

    project setup, tailwind and daisyUI added, auction.json file created and navbar design completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <Navbar/>
+        <Banner/>
+      </header>
+    </div>
+  )
+}
+
+export default App
